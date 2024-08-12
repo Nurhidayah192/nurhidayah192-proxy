520658b71e8bee1158b147477f4d7e64a693f9e5
nurhidayah192 <inurhidayana@gmail.com>
2024-08-12 22:34:04 +0700

    Proxy

---
+++ README.md
@@ -0,0 +1,1 @@
+# nurhidayah192-proxy
---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x88D11939B4194FaD820956080FFa6D8BbbA1BbBc'
+export const ProxyAdmin = '0xCa0393F23a28bE3231474D59a1f7fCe70DE8d361'
+export const SWTRImplementation = '0x792eebaA706DAC00D5605Ce2D8DfD8847c95CF2e'
+    
